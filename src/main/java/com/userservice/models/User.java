7fbb13fe6af2cea08662404c81c5cdf22b84952b
trpoliSo <tryznapalina@gmail.com>
2025-11-08 12:05:11 +0300

    alteration in userSpecification

--- src/main/java/com/userservice/models/User.java
+++ src/main/java/com/userservice/models/User.java
@@ -1,6 +1,7 @@
 package com.userservice.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,7 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class User {
 
     @Id
