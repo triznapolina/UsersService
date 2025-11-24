e7a2d146cd01711f75529a9a368e523d489c5ca7
triznapolina <polinatrizna8@gmail.com>
2025-11-24 16:48:09 +0300

    Revert "Revert "Completing CI Pipeline""

--- src/main/java/com/userservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.userservice.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
