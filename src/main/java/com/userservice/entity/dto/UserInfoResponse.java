0f2bca011ff42a16c63e5f79a3849d89d0e9974b
trpoliSo <tryznapalina@gmail.com>
2025-11-27 13:56:07 +0300

    alteration

---
+++ src/main/java/com/userservice/entity/dto/UserInfoResponse.java
@@ -0,0 +1,15 @@
+package com.userservice.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoResponse {
+    private String username;
+    private String role;
+}
