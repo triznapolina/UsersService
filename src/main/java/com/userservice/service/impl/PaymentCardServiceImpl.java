73b0ee519e5fad4db2f69bd6d95a86dd07f4bd4e
trpoliSo <tryznapalina@gmail.com>
2025-11-27 22:53:47 +0300

    alteration

--- src/main/java/com/userservice/service/impl/PaymentCardServiceImpl.java
+++ src/main/java/com/userservice/service/impl/PaymentCardServiceImpl.java
@@ -104,7 +104,7 @@ public class PaymentCardServiceImpl implements PaymentCardService {
 
     @Transactional
     @CachePut(
-            value = "user",
+            value = "card",
             key = "#id"
     )
     @Override
